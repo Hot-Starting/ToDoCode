b1bfae68aa1572364088183a30830704a9f39da8
Gotprgmer <gofiction95@gmail.com>
2023-06-08 00:18:31 +0900

    [BE] Create: 프로젝트 시작

---
+++ todocode/src/main/java/com/hotstarting/todocode/TodocodeApplication.java
@@ -0,0 +1,13 @@
+package com.hotstarting.todocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodocodeApplication.class, args);
+	}
+
+}
---
+++ todocode/src/test/java/com/hotstarting/todocode/TodocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotstarting.todocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
