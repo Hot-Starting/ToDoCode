e0f023d4102b1f45a9ca6b4906321783945e5a0f
oth54477 <oth5447@naver.com>
2023-08-02 14:14:30 +0900

    [FE] Fix : 로그인 테스트용

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -2,15 +2,13 @@ import axios from "axios";
 import Image from "next/image";
 
 export default function Login() {
-  const redirectUrl = "http://localhost:3000";
+  const redirectUrl = "http://localhost:3000/login/callback";
 
   // const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_ID}&redirect_uri=${redirectUrl}&response_type=code&scope=email profile`;
-  const googleUrl =
-    "http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/login/callback";
+  const googleUrl = `http://localhost:8080/oauth2/authorization/google?redirect_uri=${redirectUrl}`;
 
   // const githubUrl = `/github/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_ID}&redirect_url=${redirectUrl}`;
-  const githubUrl =
-    "http://localhost:8080/oauth2/authorization/github?redirect_uri=http://localhost:3000/login/callback";
+  const githubUrl = `http://localhost:8080/oauth2/authorization/github?redirect_uri=${redirectUrl}`;
 
   return (
     <>
@@ -21,7 +19,7 @@ export default function Login() {
         <a href={githubUrl}>깃허브</a>
       </div>
       <hr />
-      <div>
+      {/* <div>
         <h1>GET 요청</h1>
         <Image
           src="/googleImg.png"
@@ -50,7 +48,7 @@ export default function Login() {
             }).then((response) => console.log(response));
           }}
         />
-      </div>
+      </div> */}
     </>
   );
 }
