634997869a6efd74691d0d45b9a067c39ab7fc75
Gotprgmer <gofiction95@gmail.com>
2023-08-29 17:09:32 +0900

    [BE] Update: /api/member 컨트롤러에서 @Authentication 어노테이션으로 Principal 객체 가져오기

--- todocode/src/main/java/com/hotstarting/todocode/domain/member/controller/MemberController.java
+++ todocode/src/main/java/com/hotstarting/todocode/domain/member/controller/MemberController.java
@@ -2,11 +2,14 @@ package com.hotstarting.todocode.domain.member.controller;
 
 import com.hotstarting.todocode.domain.member.domain.Member;
 import com.hotstarting.todocode.domain.member.service.MemberService;
+import com.hotstarting.todocode.global.oauth.domain.PrincipalDetails;
 import com.hotstarting.todocode.global.response.ResponseDTO;
 import com.hotstarting.todocode.global.util.Msg;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +22,11 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping
-    public ResponseEntity<ResponseDTO> getUser() {
-        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ResponseEntity<ResponseDTO> getUser(
+            @AuthenticationPrincipal PrincipalDetails userPrincipal
+    ) {
 
-        Member member = memberService.getMember(principal.getUsername());
+        Member member = memberService.getMember(userPrincipal.getName());
         // 추후 MemberDTO 따로 만들기
         ResponseDTO responseDTO = ResponseDTO.builder().status("SUCCESS").message(Msg.SUCCESS_MEMBER_INFO).data(member).build();
 
